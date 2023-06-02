41a216ece03e215e1ce7c40ffffd33461e7ae3fe
Edo <edgar.lecuyer@epitech.eu>
2023-06-02 14:08:00 +0200

    feat(graphic): disable cursor for camera

---
+++ include/gui/Mateyak/Sprite.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** Sprite
+*/
+
+//
+// Created by Edo on 6/2/23.
+//
+
+#pragma once
+
+
+class Sprite {
+
+};
---
+++ src/gui/Mateyak/Sprite.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2023
+** Zappy
+** File description:
+** Sprite
+*/
+
+//
+// Created by Edo on 6/2/23.
+//
+
+#include "Sprite.hpp"
