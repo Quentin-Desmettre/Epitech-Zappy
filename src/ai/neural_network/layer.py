6ee4d5a7f4222c66a995adf2c9b4a636a0973579
Clément <clement.vandeville@epitech.eu>
2023-06-06 12:02:58 +0200

    fix(ai-nn): Dense layer now uses Matrix instead of list[] and biases are initialized to 1 to avoid dead network

--- src/ai/neural_network/layer.py
+++ src/ai/neural_network/layer.py
@@ -1,5 +1,5 @@
 import random
-from src.ai.neural_network.math import *
+from src.ai.neural_network.mymath import *
 
 
 class Layer:
@@ -9,11 +9,11 @@ class Layer:
         self.neuronsNumber: int = neuronsNumber
 
         self.weights: list[list[float]] = [[random.uniform(-0.01, 0.01) for _ in range(neuronsNumber)] for _ in range(inputNumber)]
-        self.bias: list[float] = [0 for _ in range(neuronsNumber)]
+        self.bias: list[float] = [1 for _ in range(neuronsNumber)]
 
-    def forward(self, inputs: list[list[float]]):
+    def forward(self, inputs: Matrix):
         try:
-            self.output = dot(Matrix(inputs), Matrix(self.weights)) + self.bias
+            self.output = dot(inputs, Matrix(self.weights)) + self.bias
         except Exception as e:
             print(e)
             self.output = None
