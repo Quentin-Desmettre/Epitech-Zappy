8104b067ffac4f50be460ead81ef504d4d033c57
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-06-10 11:45:50 +0200

    feat(server): Handled all gui requests

--- src/server/trantor/gui_handling/handlers1.c
+++ src/server/trantor/gui_handling/handlers1.c
@@ -6,24 +6,54 @@
 */
 
 #include "server.h"
+#include "utility/strings.h"
 
-char *gui_map_size_handler(server_t *server, char **args)
+char *gui_map_size_handler(server_t *server, const char *cmd)
 {
+    if (strcmp(cmd, "msz") != 0)
+        return ERROR_CODE;
+    return my_asprintf("msz %d %d",
+        server->params.width, server->params.height);
 }
 
-char *gui_tile_content_handler(server_t *server, char **args)
+static char *get_tile_content_str(server_t *server, int x, int y)
 {
+    map_tile_t *tile = get_tile_by_pos(server->trantor->map, x, y);
+
+    return my_asprintf("bct %d %d %d %d %d %d %d %d %d", x, y,
+        tile->resources[0], tile->resources[1], tile->resources[2],
+        tile->resources[3], tile->resources[4], tile->resources[5],
+        tile->resources[6]);
 }
 
-char *gui_tiles_content_handler(server_t *server, char **args)
+char *gui_tile_content_handler(server_t *server, const char *cmd)
 {
-    return my_strdup("ok\n");
+    int x;
+    int y;
+
+    if (sscanf(cmd, "bct %d %d", &x, &y) != 2)
+        return ERROR_CODE;
+    if (x < 0 || x >= server->params.width ||
+    y < 0 || y >= server->params.height)
+        return ERROR_CODE;
+    return get_tile_content_str(server, x, y);
 }
 
-char *gui_team_names_handler(server_t *server, char **args)
+char *gui_tiles_content_handler(server_t *server, const char *cmd)
 {
-}
+    char *answer = NULL;
+    size_t len = 0;
+    char *tmp;
 
-char *gui_player_position_handler(server_t *server, char **args)
-{
+    if (strcmp(cmd, "mct") != 0)
+        return ERROR_CODE;
+    for (int y = 0; y < server->params.height; y++) {
+        for (int x = 0; x < server->params.width; x++) {
+            tmp = get_tile_content_str(server, x, y);
+            str_append_free(&answer, &len, tmp);
+            str_append_free(&answer, &len, my_strdup("\n"));
+        }
+    }
+    answer[len - 1] = '\0';
+    return answer;
 }
--- src/server/trantor/gui_handling/handlers2.c
+++ src/server/trantor/gui_handling/handlers2.c
@@ -6,19 +6,70 @@
 */
 
 #include "server.h"
+#include "utility/strings.h"
+#include "args.h"
 
-char *gui_player_level_handler(server_t *server, char **args)
+client_t *get_client_by_id(server_t *server, int id)
 {
+    list_t *tmp = server->clients;
+
+    if (!server->clients)
+        return NULL;
+    do {
+        if (((client_t *)tmp->data)->data &&
+        ((client_t *)tmp->data)->data->id == id)
+            return tmp->data;
+        tmp = tmp->next;
+    } while (tmp != server->clients);
+    return NULL;
 }
 
-char *gui_player_inventory_handler(server_t *server, char **args)
+char *gui_player_level_handler(server_t *server, const char *cmd)
 {
+    int player_id;
+    client_t *cli;
+
+    if (sscanf(cmd, "plv #%d", &player_id) != 1)
+        return ERROR_CODE;
+    cli = get_client_by_id(server, player_id);
+    if (!cli)
+        return ERROR_CODE;
+    return my_asprintf("plv %d %d", cli->data->id, cli->data->level);
 }
 
-char *gui_time_request_handler(server_t *server, char **args)
+char *gui_player_inventory_handler(server_t *server, const char *cmd)
 {
+    int player_id;
+    client_t *cli;
+    player_t *player;
+
+    if (sscanf(cmd, "pin #%d", &player_id) != 1)
+        return ERROR_CODE;
+    cli = get_client_by_id(server, player_id);
+    if (!cli)
+        return ERROR_CODE;
+    player = cli->data;
+    return my_asprintf("pin %d %d %d %d %d %d %d %d %d %d", player_id,
+        player->x, player->y, player->inventory[0], player->inventory[1],
+        player->inventory[2], player->inventory[3], player->inventory[4],
+        player->inventory[5], player->inventory[6]);
 }
 
-char *gui_time_change_handler(server_t *server, char **args)
+char *gui_time_request_handler(server_t *server, const char *cmd)
 {
+    if (strcmp(cmd, "sgt") != 0)
+        return ERROR_CODE;
+    return my_asprintf("sgt %d", server->params.freq);
+}
+
+char *gui_time_change_handler(server_t *server, const char *cmd)
+{
+    int new_freq;
+
+    if (sscanf(cmd, "sst %d", &new_freq) != 1)
+        return ERROR_CODE;
+    if (!IS_FREQ_VALID(new_freq))
+        return ERROR_CODE;
+    server->params.freq = new_freq;
+    return my_asprintf("sst %d", server->params.freq);
 }
