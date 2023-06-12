3502899a6288b807c5a0f391ce4669b38beb9c2e
Talleux <ewen.talleux@epitech.eu>
2023-06-12 11:11:50 +0200

    fix(server): remove test lines in init trantor

--- src/server/trantor/trantor_init.c
+++ src/server/trantor/trantor_init.c
@@ -33,10 +33,7 @@ trantor_t *init_trantor(int width, int height,
 
     trantor->map = init_map(width, height);
     trantor->width = width;
-    trantor->height = height + 1;
-    server_t *server = malloc(sizeof(server_t));
-    server->trantor = trantor;
-    ai_broadcast_handler(NULL, server, NULL);
+    trantor->height = height;
     for (int i = 0; team_names[i]; i++)
         append_node(&trantor->teams, create_team(team_names[i], max_players));
     return trantor;
