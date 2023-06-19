9559ecf924ed56d05453b226fa1a5abe03faca0d
TalleuxEwen <ewen.talleux@epitech.eu>
2023-06-19 18:28:55 +0200

    feat(gui): add phaser/flange effect for 3D audio immersion

--- src/gui/Mateyak/Audio.cpp
+++ src/gui/Mateyak/Audio.cpp
@@ -51,12 +51,12 @@ void Mateyak::Audio::playSound()
     if (result != FMOD_OK)
         std::cout << "Error while adding DSP : " << FMOD_ErrorString(result) << std::endl;
     _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_DECAYTIME, 100.0f);
-    /*result = _system->createDSPByType(FMOD_DSP_TYPE_FLANGE, &_dspPhaser);
+    result = _system->createDSPByType(FMOD_DSP_TYPE_FLANGE, &_dspPhaser);
     if (result != FMOD_OK)
         std::cout << "Error while creating DSP" << std::endl;
     result = _channel->addDSP(0, _dspPhaser);
     if (result != FMOD_OK)
-        std::cout << "Error while adding DSP : " << FMOD_ErrorString(result) << std::endl;*/
+        std::cout << "Error while adding DSP : " << FMOD_ErrorString(result) << std::endl;
 }
 
 void Mateyak::Audio::stopSound() const
@@ -159,17 +159,29 @@ void Mateyak::Audio::setEffects(float angle) const
     if (angle >= 0 && angle <= 90) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 5000.0f + ((angle) * ((22000.0f - 5000.0f) / 90)));
         _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 100.0f - ((angle) * (100.0f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_MIX, 25.0f - ((angle) * (25.0f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_DEPTH, 0.5f - ((angle) * (0.5f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_RATE, 10.f - ((angle) * (10.f / 90)));
     }
     if (angle <= 360 && angle > 270) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f - ((angle - 270) * ((22000.0f - 5000.0f) / 90)));
         _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f + ((angle - 270) * (100.0f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_MIX, 0.0f + ((angle - 270) * (25.0f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_DEPTH, 0.0f + ((angle - 270) * (0.5f / 90)));
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_RATE, 0.0f + ((angle - 270) * (10.0f / 90)));
     }
     if (angle > 90 && angle <= 180) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f);
         _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_MIX, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_DEPTH, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_RATE, 0.f);
     }
     if (angle > 180 && angle <= 270) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f);
         _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_MIX, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_DEPTH, 0.0f);
+        _dspPhaser->setParameterFloat(FMOD_DSP_FLANGE_RATE, 0.f);
     }
 }
