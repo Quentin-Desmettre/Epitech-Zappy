347d0c743163ccecd45a833e28cfdd3ead0684ce
TalleuxEwen <ewen.talleux@epitech.eu>
2023-06-19 18:01:40 +0200

    feat(gui): fix reverb for 3D effect

--- src/gui/Mateyak/Audio.cpp
+++ src/gui/Mateyak/Audio.cpp
@@ -44,14 +44,14 @@ void Mateyak::Audio::playSound()
     if (result != FMOD_OK)
         std::cout << "Error while adding DSP : " << FMOD_ErrorString(result) << std::endl;
 
-    /*result = _system->createDSPByType(FMOD_DSP_TYPE_ECHO, &_dspReverb);
+    result = _system->createDSPByType(FMOD_DSP_TYPE_SFXREVERB, &_dspReverb);
     if (result != FMOD_OK)
         std::cout << "Error while creating DSP" << std::endl;
     result = _channel->addDSP(0, _dspReverb);
     if (result != FMOD_OK)
         std::cout << "Error while adding DSP : " << FMOD_ErrorString(result) << std::endl;
-
-    result = _system->createDSPByType(FMOD_DSP_TYPE_FLANGE, &_dspPhaser);
+    _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_DECAYTIME, 100.0f);
+    /*result = _system->createDSPByType(FMOD_DSP_TYPE_FLANGE, &_dspPhaser);
     if (result != FMOD_OK)
         std::cout << "Error while creating DSP" << std::endl;
     result = _channel->addDSP(0, _dspPhaser);
@@ -158,18 +158,18 @@ void Mateyak::Audio::setEffects(float angle) const
 {
     if (angle >= 0 && angle <= 90) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 5000.0f + ((angle) * ((22000.0f - 5000.0f) / 90)));
-        std::cout << "filter : " << 5000.0f + ((angle) * ((22000.0f - 5000.0f) / 90)) << std::endl;
+        _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 100.0f - ((angle) * (100.0f / 90)));
     }
     if (angle <= 360 && angle > 270) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f - ((angle - 270) * ((22000.0f - 5000.0f) / 90)));
-        std::cout << "filter : " << 22000.0f - ((angle - 270) * ((22000.0f - 5000.0f) / 90)) << std::endl;
+        _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f + ((angle - 270) * (100.0f / 90)));
     }
     if (angle > 90 && angle <= 180) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f);
-        std::cout << "filter : " << 22000.0f << std::endl;
+        _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f);
     }
     if (angle > 180 && angle <= 270) {
         _dspFilter->setParameterFloat(FMOD_DSP_LOWPASS_CUTOFF, 22000.0f);
-        std::cout << "filter : " << 22000.0f << std::endl;
+        _dspReverb->setParameterFloat(FMOD_DSP_SFXREVERB_EARLYDELAY, 0.0f);
     }
 }
