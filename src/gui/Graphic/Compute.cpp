69aaa6c454488820941fbd32d4df02db3505bd7f
Edo <edgar.lecuyer@epitech.eu>
2023-06-20 16:19:33 +0200

    fix(graphic): fixed pos of _cam's def

--- src/gui/Graphic/Compute.cpp
+++ src/gui/Graphic/Compute.cpp
@@ -26,8 +26,8 @@ Graphic::Graphic(Mateyak::Vec2f mapSize, Mateyak::Vec2f windowSize, ServerInform
     _windowWidth(windowSize.x), _windowHeight(windowSize.y),
     _mapSize(mapSize),
     _win(_windowWidth, _windowHeight, "Zappy", 400),
-    _map(mapSize * 10, 0.5),
     _cam({0.0f, 8.0f, 0.0f}, {mapSize.x * 5 / 3.f, 0.5f, mapSize.y * 5 / 3.f}, {0.0f, 1.0f, 0.0f}, 45.0f),
+    _map(mapSize * 10, 0.5),
     _flat(GenMeshPoly(10, 10000.0f), Mateyak::Vec3f{-500, -1, -500}, 1.0f, BLACK),
     _shader("src/gui/shader/base_lighting.vs", "src/gui/shader/test.fs")
 {
