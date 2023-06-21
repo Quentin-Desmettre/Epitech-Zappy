4c630e3f49273b73d63f92c74ea2ab253121484f
Noa <noa.trachez@epitech.eu>
2023-06-21 12:58:09 +0200

    feat(graphic): Usage message

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -118,6 +118,9 @@ int main(int ac, char **av)
        }
     } catch (const std::exception& ex) {
         std::cerr << "Exception: " << ex.what() << std::endl;
+        std::cout << "USAGE:\t./zappy_ai -p port -h machine" << std::endl;
+        std::cout << "\tport\tis the port number" << std::endl;
+        std::cout << "\tmachine\tis the name of the machine; localhost by default" << std::endl;
     }
     return 0;
 }
