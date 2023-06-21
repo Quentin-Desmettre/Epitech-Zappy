69ed211a019466aa26d442af592196444e601e28
PaulinLec <paulin.leclercq@epitech.eu>
2023-06-21 15:32:49 +0200

    feat(tests): tests for _movement.py and shorter path when going in the directions 4 and 6 (bottom corners)

--- tests/ai/test_main.py
+++ tests/ai/test_main.py
@@ -4,6 +4,7 @@ from tests.ai.test_finding import FindingTester
 from tests.ai.test_commands import CommandTester
 from tests.ai.test_reader import ReaderTester
 from tests.ai.test_utils import UtilsTester
+from tests.ai.test_movement import MovementTester
 
 
 if __name__ == "__main__": # pragma: no cover
---
+++ tests/ai/test_movement.py
@@ -0,0 +1,69 @@
+import sys
+from os import remove
+from unittest import TestCase
+from tests.ai.globals import ai, server
+from src.ai.commands import Directions
+from src.ai.utils import set_verbose, Colors
+
+
+class MovementTester(TestCase):
+
+    def setUp(self):
+        global ai
+        self.ai = ai
+        sys.stdout = open("movement_output", "w+", buffering=1)
+        self.output = open("movement_output", "r+")
+        set_verbose(True)
+
+    def tearDown(self):
+        global server
+        server.reset()
+        sys.stdout.close()
+        sys.stdout = sys.__stdout__
+        self.output.close()
+        remove("movement_output")
+
+    def test_move_randomly(self):
+        self.ai.move_randomly()
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 1
+                        or output.count("Sending: Right") == 1
+                        or output.count("Sending: Left") == 1)
+
+    def test_go_to_direction(self):
+        self.ai.go_to_direction(Directions.FORWARD)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 1)
+
+        self.ai.go_to_direction(Directions.RIGHT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Right") == 1)
+        self.assertTrue(output.count("Sending: Forward") == 1)
+
+        self.ai.go_to_direction(Directions.LEFT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Left") == 1)
+        self.assertTrue(output.count("Sending: Forward") == 1)
+
+        self.ai.go_to_direction(Directions.BACKWARD)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Left") == 2)
+        self.assertTrue(output.count("Sending: Forward") == 1)
+
+        self.ai.go_to_direction(Directions.TOP_RIGHT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 2 and output.count("Sending: Right") == 1)
+
+        self.ai.go_to_direction(Directions.TOP_LEFT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 2 and output.count("Sending: Left") == 1)
+
+        self.ai.go_to_direction(Directions.BOTTOM_RIGHT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 2)
+        self.assertTrue(output.count("Sending: Right") == 2)
+
+        self.ai.go_to_direction(Directions.BOTTOM_LEFT)
+        output = self.output.read()
+        self.assertTrue(output.count("Sending: Forward") == 2)
+        self.assertTrue(output.count("Sending: Left") == 2)
